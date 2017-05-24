d4a4cbf15dc4fe9055f1e7123c1932a8342ac0fc
deepakchampatiray <deepakchampatiray@gmail.com>
2017-05-24 03:18:09 -0400

    Completed - Using orderBy and limitTo filters

--- src/app.js
+++ src/app.js
@@ -4,21 +4,65 @@
     angular.module("myApp", []);
 
     angular.module("myApp")
-    .controller("customerController", [
+    .controller("usersController", [
       function(){
-        this.firstName = "hello world";
-        this.lastName = "champatiray";
-        this.accountType = "savings";
-        this.accountBalance = 1234.56;
+        this.userList = [
+                      {
+                        "firstName": "Leonor",
+                        "lastName": "Fleming",
+                        "email": "leonorfleming@telpod.com"
+                      },
+                      {
+                        "firstName": "Berg",
+                        "lastName": "York",
+                        "email": "bergyork@telpod.com"
+                      },
+                      {
+                        "firstName": "Virginia",
+                        "lastName": "Lindsay",
+                        "email": "virginialindsay@telpod.com"
+                      },
+                      {
+                        "firstName": "Rosalind",
+                        "lastName": "Burris",
+                        "email": "rosalindburris@telpod.com"
+                      },
+                      {
+                        "firstName": "Clare",
+                        "lastName": "Beasley",
+                        "email": "clarebeasley@telpod.com"
+                      },
+                      {
+                        "firstName": "Bowen",
+                        "lastName": "Black",
+                        "email": "bowenblack@telpod.com"
+                      },
+                      {
+                        "firstName": "Ferguson",
+                        "lastName": "Bush",
+                        "email": "fergusonbush@telpod.com"
+                      },
+                      {
+                        "firstName": "Lawson",
+                        "lastName": "Lang",
+                        "email": "lawsonlang@telpod.com"
+                      },
+                      {
+                        "firstName": "Angie",
+                        "lastName": "Hurst",
+                        "email": "angiehurst@telpod.com"
+                      },
+                      {
+                        "firstName": "Clarke",
+                        "lastName": "England",
+                        "email": "clarkeengland@telpod.com"
+                      },
+                      {
+                        "firstName": "Fleming",
+                        "lastName": "Walker",
+                        "email": "flemingwalker@telpod.com"
+                      }
+                    ]
       }
-    ])
-    .filter("capitalize", [function(){
-      return function(val) {
-        return val.split(" ")
-              .map(function(str) {
-                return str[0].toUpperCase()+str.substring(1).toLowerCase();
-              })
-              .join(" ");
-      }
-    }])
+    ]);
 })();
