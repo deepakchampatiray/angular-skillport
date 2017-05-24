77b2485d4b0c59116d24ef9903207e302ddad55a
deepakchampatiray <deepakchampatiray@gmail.com>
2017-05-24 01:13:05 -0400

    Completed - Using View Filters

--- src/app.js
+++ src/app.js
@@ -3,34 +3,13 @@
 (function() {
     angular.module("myApp", []);
 
-    angular.module("myApp").service("SharedService", function(){
-      return {name: 'Sneha'};
-    })
-
-    angular.module("myApp").controller("parentController",[
-      '$scope',
-      function($scope){
-        $scope.model = {name : 'deeapk'}
+    angular.module("myApp")
+    .controller("customerController", [
+      function(){
+        this.firstName = "Deepak";
+        this.lastName = "Champatiray";
+        this.accountType = "Savings";
+        this.accountBalance = 1234.56;
       }
-    ])
-    .controller("controller1", [
-      '$scope',
-      function($scope){}
-    ])
-    .controller("controller2", [
-      '$scope',
-      function($scope){}
-    ])
-    .controller('controller3', [
-      '$scope', 'SharedService',
-      function($scope, SharedService) {
-        $scope.model = SharedService;
-      }
-    ])
-    .controller('controller4', [
-      '$scope', 'SharedService',
-      function($scope, SharedService) {
-        $scope.model = SharedService;
-      }
-    ])
+    ]);
 })();
