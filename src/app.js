fc21de268b4b77c462a9a84d133d2a8a6042e5c7
deepakchampatiray <deepakchampatiray@gmail.com>
2017-05-24 02:26:11 -0400

    Completed - Creating own angular filters.

--- src/app.js
+++ src/app.js
@@ -6,10 +6,19 @@
     angular.module("myApp")
     .controller("customerController", [
       function(){
-        this.firstName = "Deepak";
-        this.lastName = "Champatiray";
-        this.accountType = "Savings";
+        this.firstName = "hello world";
+        this.lastName = "champatiray";
+        this.accountType = "savings";
         this.accountBalance = 1234.56;
       }
-    ]);
+    ])
+    .filter("capitalize", [function(){
+      return function(val) {
+        return val.split(" ")
+              .map(function(str) {
+                return str[0].toUpperCase()+str.substring(1).toLowerCase();
+              })
+              .join(" ");
+      }
+    }])
 })();
