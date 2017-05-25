3a72a649c15bd716d7b3b570ad02ef1f7e297002
deepakchampatiray <deepakchampatiray@gmail.com>
2017-05-25 01:14:19 -0400

    Completed - Number and json filter

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@
     angular.module("myApp")
     .controller("usersController", [
       function(){
+        this.num = 123456.789;
         this.userList = [
                       {
                         "firstName": "Leonor",
@@ -21,46 +22,6 @@
                         "firstName": "Virginia",
                         "lastName": "Lindsay",
                         "email": "virginialindsay@telpod.com"
-                      },
-                      {
-                        "firstName": "Rosalind",
-                        "lastName": "Burris",
-                        "email": "rosalindburris@telpod.com"
-                      },
-                      {
-                        "firstName": "Clare",
-                        "lastName": "Beasley",
-                        "email": "clarebeasley@telpod.com"
-                      },
-                      {
-                        "firstName": "Bowen",
-                        "lastName": "Black",
-                        "email": "bowenblack@telpod.com"
-                      },
-                      {
-                        "firstName": "Ferguson",
-                        "lastName": "Bush",
-                        "email": "fergusonbush@telpod.com"
-                      },
-                      {
-                        "firstName": "Lawson",
-                        "lastName": "Lang",
-                        "email": "lawsonlang@telpod.com"
-                      },
-                      {
-                        "firstName": "Angie",
-                        "lastName": "Hurst",
-                        "email": "angiehurst@telpod.com"
-                      },
-                      {
-                        "firstName": "Clarke",
-                        "lastName": "England",
-                        "email": "clarkeengland@telpod.com"
-                      },
-                      {
-                        "firstName": "Fleming",
-                        "lastName": "Walker",
-                        "email": "flemingwalker@telpod.com"
                       }
                     ]
       }
