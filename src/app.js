c722441d34aa107b178befdb0e77e9f49d0ed831
deepakchampatiray <deepakchampatiray@gmail.com>
2017-05-26 01:39:00 -0400

    Completed - Date filters

--- src/app.js
+++ src/app.js
@@ -6,24 +6,11 @@
     angular.module("myApp")
     .controller("usersController", [
       function(){
-        this.num = 123456.789;
-        this.userList = [
-                      {
-                        "firstName": "Leonor",
-                        "lastName": "Fleming",
-                        "email": "leonorfleming@telpod.com"
-                      },
-                      {
-                        "firstName": "Berg",
-                        "lastName": "York",
-                        "email": "bergyork@telpod.com"
-                      },
-                      {
-                        "firstName": "Virginia",
-                        "lastName": "Lindsay",
-                        "email": "virginialindsay@telpod.com"
-                      }
-                    ]
+        this.user = {
+          "firstName": "Cherie",
+          "lastName": "Anderson",
+          "birthDate": 1043333088435
+        }
       }
     ]);
 })();
