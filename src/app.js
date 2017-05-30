f4a757589c81751afde85bd25611378bbdb13032
deepakchampatiray <deepakchampatiray@gmail.com>
2017-05-30 03:21:04 -0400

    Completed - Angular iterators.

--- src/app.js
+++ src/app.js
@@ -142,75 +142,14 @@
               }
             ];
 
-          function getUserNames(userId){
-            var deferred = $q.defer();
-
-            setTimeout(function(){
-              if(vm.travelHistory[userId]) {
-                deferred.resolve(
-                  {'firstName' : vm.travelHistory[userId].firstName,
-                    'lastName' : vm.travelHistory[userId].lastName
-                  });
-              }
-              else {
-                deferred.reject("User Not found");
-              }
-            },2000);
-
-            return deferred.promise;
-          }
-
-          function getTravelHistory(userId){
-            var deferred = $q.defer();
-
-            setTimeout(function(){
-              if(vm.travelHistory[userId] && vm.travelHistory[userId].travelHistory) {
-                deferred.resolve(vm.travelHistory[userId].travelHistory);
-              }
-              else {
-                deferred.reject("Travel History Not found");
-              }
-            },4000);
-
-            return deferred.promise;
-          }
-
-          vm.userId = null;
-
-          vm.fetchRecord = function(){
-            if(vm.userId || vm.userId === 0) {
-              vm.message = "Fetching record for " + vm.userId;
-              vm.firstName = vm.lastName = vm.history = null;
-
-              /**
-                Since the two calls have different timings, there is a delay between the appearance of one result and the other.
-
-              getUserNames(vm.userId)
-                .then(function(user) {
-                  vm.firstName = user.firstName;
-                  vm.lastName = user.lastName;
-                  vm.message = "Fetched first and last name for user Id " + vm.userId;
-                });
-
-              getTravelHistory(vm.userId)
-                .then(function(travelHistory){
-                  vm.history = travelHistory;
-                });
-              */
-              /*
-                To Display result only after all the calls have finished
-              */
-
-              $q.all([getUserNames(vm.userId), getTravelHistory(vm.userId)])
-               .then(function(resultArr){
-                 vm.firstName = resultArr[0].firstName;
-                 vm.lastName = resultArr[0].lastName;
-                 vm.history = resultArr[1];
-                 vm.message = "Fetched records for user Id " + vm.userId;
-               })
-               .catch(function(message){
-                 vm.message = message;
-               })
+          vm.countries = function(ctryArr) {
+            //console.log(ctryArr);
+            if(ctryArr) {
+              var countries = [];
+              angular.forEach(ctryArr, function(ctry){
+                countries.push(ctry.country);
+              });
+              return countries.join(", ");
             }
           }
       }
