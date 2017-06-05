7e8b24713073554f74c4e5ec4202e37ea3a42c06
deepakchampatiray <deepakchampatiray@gmail.com>
2017-06-05 01:56:55 -0400

    Completed - Examining isolate scope in directives

--- src/app.js
+++ src/app.js
@@ -4,11 +4,41 @@
     angular.module("myApp", []);
 
     angular.module("myApp")
-    .controller("mouseController", [
-      function(){
-          var vm = this;
-          vm.isOverImage = false;
-          
+    .controller("carController", ['$scope',
+      function($scope){
+          $scope.car = {
+            name : 'Tesla',
+            model : 'Model S',
+            make : 2017,
+            type : 'Electric'
+          }
+
+          $scope.model3 = {
+            name : 'Tesla',
+            model : 'Model 3',
+            make : 2018,
+            type : 'Electric'
+          }
+
+          $scope.modelT = {
+            name : 'Tesla',
+            model : 'Model T',
+            make : 2017,
+            type : 'Electric SUV'
+          }
       }
     ])
+    .directive('dcrCar', function() {
+      return {
+        'templateUrl' : "templates/dcrCar.html"
+      }
+    })
+    .directive("dcrCarIsolate", function(){
+      return {
+        scope : {
+          car : "="
+        },
+        templateUrl : "templates/dcrCar.html"
+      }
+    })
 })();
