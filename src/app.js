91d2abc4bf405484c21bc7d6eb6670c9a9da970b
deepakchampatiray <deepakchampatiray@gmail.com>
2017-06-14 01:49:40 -0400

    Completed - Using ng-list directive

--- src/app.js
+++ src/app.js
@@ -4,31 +4,13 @@
     angular.module("myApp", []);
 
     angular.module("myApp")
-    // .controller("temperatureController", [
-    //   function(){
-    //       var vm = this;
-    //       vm.currentTemperature = 28;
-    //   }
-    // ])
-    .directive("colorChangingSquare", function(){
-      return {
-        scope : {
-          size : '@'
-        },
-        transclude : true,
-        replace : true,
-        templateUrl : 'templates/colorChangingSquare.html',
-        link : function(scope, elem, attrs) {
-          elem.on("mouseover", function(){
-            console.log("mouse over", elem);
-            elem.css("background-color", attrs.color);
-          });
-
-          elem.on("mouseout", function(){
-            console.log("mouse out");
-            elem.css("background-color", "");
-          });
-        }
+    .controller("userController", [
+      function(){
+          var vm = this;
+          vm.users = ['Deepak', 'Sneha', 'Jitesh', 'Sunil'];
+          vm.typeOf = function(val) {
+            return typeof(val);
+          }
       }
-    })
+    ]);
 })();
