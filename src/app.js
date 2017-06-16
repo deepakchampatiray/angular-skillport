4d5d222892326b55c5c86922b28194d5b876c1b1
deepakchampatiray <deepakchampatiray@gmail.com>
2017-06-15 23:47:52 -0400

    Completed - $timeout service

--- src/app.js
+++ src/app.js
@@ -4,14 +4,33 @@
     angular.module("myApp", []);
 
     angular.module("myApp")
-    .controller("userController", [
-      function(){
+    .controller("timerController", ['$timeout',
+      function($timeout){
           var vm = this;
-          vm.user = {
-            name : 'Deepak Champatiray',
-            salary : 1027865.76,
-            dateOfBirth : 545666700000
+          var timeoutPromise = null;
+          vm.timerValue = 20;
+          vm.startCountDown = function() {
+            countdown();
+          }
+          vm.stopCountDown = function() {
+            $timeout.cancel(timeoutPromise);
+          }
+
+          var decrementTimer = function() {
+            vm.timerValue--;
+          }
+          function countdown() {
+            if(vm.timerValue != 0) {
+              decrementTimer();
+              timeoutPromise = $timeout(countdown, 1000);
+            }
           }
       }
-    ]);
+    ])
+    .filter("timerFormatter", function(){
+      return function(timeVal){
+        //console.log(timeVal);
+        return (Math.floor(timeVal/60) + " min, ") + (timeVal % 60) + " sec";
+      }
+    });
 })();
