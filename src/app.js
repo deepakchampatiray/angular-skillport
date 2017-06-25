ef4f2d3d1ba9007c18a77a5d4c664b8b75deacf8
deepakchampatiray <deepakchampatiray@gmail.com>
2017-06-25 11:17:37 -0400

    Completed - $http.get service

--- src/app.js
+++ src/app.js
@@ -3,18 +3,30 @@
 (function() {
     angular.module("myApp", []);
 
+    angular.module("myApp").factory("TimeFactory", ['$http', function($http){
+      var model = {};
+      model.currentTime = null;
+
+      model.getTime = function() {
+        $http.get('http://date.jsontest.com/')
+            .then(function(resp) {
+                    this.currentTime = resp.data.milliseconds_since_epoch;
+                    console.log(this, resp);
+                  }.bind(model),
+                  function(data) {
+                    console.error(data);
+                  }
+                )
+      }
+
+      return model;
+    }]);
+
     angular.module("myApp")
-    .controller("errorController", [
-      function(){
+    .controller("timeController", ['TimeFactory',
+      function(timeFactory){
           var vm = this;
-          vm.throwError=function(){
-            throw new Error("A new error : " + (new Date()));
-          }
+          vm.timeFactory = timeFactory;
       }
     ])
-    .factory('$exceptionHandler', function(){
-      return function(exception, cause){
-        console.log("Custom Error Logging ", exception, cause);
-      }
-    })
 })();
