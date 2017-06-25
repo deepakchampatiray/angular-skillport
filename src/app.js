f43901a0516da659decca62fcb492aab99b081c1
deepakchampatiray <deepakchampatiray@gmail.com>
2017-06-25 04:41:07 -0400

    Completed - $exceptionHandler

--- src/app.js
+++ src/app.js
@@ -4,28 +4,17 @@
     angular.module("myApp", []);
 
     angular.module("myApp")
-    .controller("cacheController", ['$cacheFactory',
-      function($cacheFactory){
+    .controller("errorController", [
+      function(){
           var vm = this;
-          vm.cacheInst = $cacheFactory("myAppCache");
-          vm.keys = [];
-          console.log(vm.cacheInst);
-
-          vm.addItem=function(key, value) {
-              if(vm.keys.indexOf(key) === -1)
-                vm.keys.push(key);
-              vm.cacheInst.put(key, value);
-              console.log(vm.cacheInst.info(), vm.keys);
-          }
-
-          vm.removeItem=function(key) {
-            vm.keys.splice(vm.keys.indexOf(key), 1);
-            vm.cacheInst.remove(key);
-          }
-
-          vm.getItem=function(key) {
-            return vm.cacheInst.get(key);
+          vm.throwError=function(){
+            throw new Error("A new error : " + (new Date()));
           }
       }
     ])
+    .factory('$exceptionHandler', function(){
+      return function(exception, cause){
+        console.log("Custom Error Logging ", exception, cause);
+      }
+    })
 })();
